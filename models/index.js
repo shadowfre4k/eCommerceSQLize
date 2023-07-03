8b381541c5943cf06b9f31881c8a453b64db6ddc
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-07-02 21:05:48 -0700

    added relationships

--- models/index.js
+++ models/index.js
@@ -1,17 +1,28 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.hasOne(Category, {
+  foreignKey: "product_category",
+  onDelete: "CASCADE",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "product_category",
+  onDelete: "CASCADE",
+});
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  foreignKey: "product_tag",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey: "product_tag",
+});
 module.exports = {
   Product,
   Category,
