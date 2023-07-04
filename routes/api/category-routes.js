dfb3554a74c87637f00d6f9e100fb48ca6ffccf1
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-07-03 20:31:17 -0700

    Created Delete for tag and Catgegory routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,8 +45,19 @@ router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No Category found with this ID!" });
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,7 +47,16 @@ router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({ where: { id: req.params.id } });
+    if (!tagData) {
+      res.status(404).json({ message: "No Tag found with this ID!" });
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete on tag by its `id` value
 });
 
